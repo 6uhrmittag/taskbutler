f9a98d36ed48442dbc000c0a5f119b2365701fa7
Marvin <dermarvin.mobil@gmail.com>
2018-10-30 14:35:56 +0100

    initial pypi version

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/6uhrmittag/taskbutler',
-    version='2.0.4',
+    version='2.0.5',
     zip_safe=False,
 )
