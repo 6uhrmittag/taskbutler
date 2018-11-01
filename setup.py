62f1125ee0770a705b67b20770ecf9fbc85364a0
Marvin <dermarvin.mobil@gmail.com>
2018-11-01 23:09:53 +0100

    version up

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/6uhrmittag/taskbutler',
-    version='2.0.6',
+    version='2.0.7',
     zip_safe=False,
 )
