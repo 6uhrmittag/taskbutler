6d5ce5eaa4beab68c385d3628d5f070361da7469
Marvin <dermarvin.mobil@gmail.com>
2018-11-01 21:21:14 +0100

    update tests for environment

--- tests/test_test_environment.py
+++ tests/test_test_environment.py
@@ -13,12 +13,5 @@ def test_path_to_tests_directory(capsys):
     with capsys.disabled():
         print(sys.path[0])
 
-def test_path_to_tests_directory_baskslashed(capsys):
-    with capsys.disabled():
-        print(sys.path[0] + '/')
-
 def test_path_to_todoistJSON_like_API(capsys):
-    with capsys.disabled():
-        print(os.path.expanduser('todoist_testdata_before.json'))
-        print(os.path.expanduser(sys.path[0] + '/' )+ 'todoist_testdata_before.json')
-    assert path.exists(os.path.expanduser('todoist_testdata_before.json')) is True
+    assert path.exists(os.path.expanduser(os.path.expanduser(sys.path[0] + '/' )+ 'todoist_testdata_before.json')) is True
