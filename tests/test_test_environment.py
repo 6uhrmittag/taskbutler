223c49532a93871299b2f28cf30d9181159a5d1f
Marvin <dermarvin.mobil@gmail.com>
2018-11-02 21:21:05 +0100

    fix tests

--- tests/test_test_environment.py
+++ tests/test_test_environment.py
@@ -56,7 +56,7 @@ class TestCreateConfigPaths:
     def test_create_app_path(self):
         # create app
         if not os.path.exists(config.getConfigPaths().app()):
-            os.makedirs(config.getConfigPaths().app(), exist_ok=True)
+            os.makedirs(config.getConfigPaths().app(), mode=0o722, exist_ok=True)
 
         assert os.path.exists(config.getConfigPaths().app()) is True
 
@@ -65,7 +65,7 @@ class TestCreateConfigPaths:
         # create config
         while not os.path.exists(config.getConfigPaths().config()):
             if os.path.exists(config.getConfigPaths().app()) and not os.path.exists(config.getConfigPaths().config()):
-                os.makedirs(config.getConfigPaths().config(), exist_ok=True)
+                os.makedirs(config.getConfigPaths().config(), mode=0o722, exist_ok=True)
 
         assert os.path.exists(config.getConfigPaths().config()) is True
 
@@ -80,15 +80,13 @@ class TestCreateConfigPaths:
     def test_create_template_paths(self):
         # create templates
         if os.path.exists(config.getConfigPaths().app()) and not os.path.exists(config.getConfigPaths().templates()):
-            os.makedirs(config.getConfigPaths().templates(), exist_ok=True)
-
-
+            os.makedirs(config.getConfigPaths().templates(), mode=0o722, exist_ok=True)
 
         assert os.path.exists(config.getConfigPaths().templates()) is True
 
     def test_create_log_paths(self):
         # create log
         if os.path.exists(config.getConfigPaths().app()) and not os.path.exists(config.getConfigPaths().log()):
-            os.makedirs(config.getConfigPaths().log(), exist_ok=True)
+            os.makedirs(config.getConfigPaths().log(), mode=0o722, exist_ok=True)
 
         assert os.path.exists(config.getConfigPaths().log()) is True
