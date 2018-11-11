b46af3c460f23e879bb912b45d426dcfac73bc3d
Marvin <dermarvin.mobil@gmail.com>
2018-11-11 16:36:09 +0100

    Bump version: 2.0.26 → 2.0.27

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/6uhrmittag/taskbutler',
-    version='2.0.26',
+    version='2.0.27',
     zip_safe=False,
 )
