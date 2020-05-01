8bc22c85d37b1a76d053a8a4ed3df4b37a132300
Marvin Heimbrodt <marvin@6uhrmittag.de>
2020-05-01 02:31:50 +0200

    Bump version: 2.2.0 → 2.2.1

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/6uhrmittag/taskbutler',
-    version='2.2.0',
+    version='2.2.1',
     zip_safe=False,
 )
